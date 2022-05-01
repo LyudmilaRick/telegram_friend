c442e127748d4bf68c38bb7e7926343589416df4
RickLyudmila <Lrick@yandex.ru>
2022-05-01 14:40:21 +0300

    Initial commit

---
+++ src/main/java/sky/pro/telegram_friend/TelegramFriendApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.telegram_friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramFriendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramFriendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/telegram_friend/TelegramFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.telegram_friend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramFriendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
